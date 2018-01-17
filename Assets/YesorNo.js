cac7b67004b325fa7b00ab695c05cea11fa86eb0
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:57:07 -0500

    Added App
    This is the first version of Yes or No that GitHub has ever seen!

---
+++ Assets/YesorNo.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var Word : UnityEngine.UI.Text;
+var Press : GameObject;
+
+function Start () {
+	Word.text = "";
+	Press.SetActive (true);
+}
+
+function yesorno () {
+	var number = Random.Range(1,3);
+	Press.SetActive (false);
+	if (number == 1) {
+		Word.text = "Yes!";
+	}else if (number == 2) {
+		Word.text = "No!";
+	}
+}
